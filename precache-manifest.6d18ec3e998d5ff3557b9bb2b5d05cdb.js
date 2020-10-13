763e7040fd29fa1ddf92a36e744fc1e11d1c8eb7
sangmin802 <56113148+sangmin802@users.noreply.github.com>
2020-10-13 18:59:59 +0900

    Updates

---
+++ precache-manifest.6d18ec3e998d5ff3557b9bb2b5d05cdb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96680ae872ce3dc294d556ff7b4879ff",
+    "url": "/loa-hands/index.html"
+  },
+  {
+    "revision": "cb7c4f4ac15ed63f0072",
+    "url": "/loa-hands/static/css/main.548fb6e1.chunk.css"
+  },
+  {
+    "revision": "33ebd56d7464093ba5da",
+    "url": "/loa-hands/static/js/2.c39d71e7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/loa-hands/static/js/2.c39d71e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb7c4f4ac15ed63f0072",
+    "url": "/loa-hands/static/js/main.10997d30.chunk.js"
+  },
+  {
+    "revision": "b895c641323837b7fd48",
+    "url": "/loa-hands/static/js/runtime-main.02ec9361.js"
+  }
+]);
