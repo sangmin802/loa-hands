2acd12f8bde382711cce8a30148f5216eaf07563
sangmin802 <56113148+sangmin802@users.noreply.github.com>
2020-10-20 19:23:10 +0900

    Updates

---
+++ precache-manifest.9ae08ef5719306131080c9639012fb06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7c14fd7629ffc79148c7d6b13068ae2",
+    "url": "/loa-hands/index.html"
+  },
+  {
+    "revision": "1434edf9612f304cd05f",
+    "url": "/loa-hands/static/css/main.730a6818.chunk.css"
+  },
+  {
+    "revision": "48840a2dad76c475b948",
+    "url": "/loa-hands/static/js/2.552453f8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/loa-hands/static/js/2.552453f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1434edf9612f304cd05f",
+    "url": "/loa-hands/static/js/main.b7d0c547.chunk.js"
+  },
+  {
+    "revision": "b895c641323837b7fd48",
+    "url": "/loa-hands/static/js/runtime-main.02ec9361.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/loa-hands/precache-manifest.e2d2dbd749fd9a4e72b4fdc954748dd8.js"
+  "/loa-hands/precache-manifest.9ae08ef5719306131080c9639012fb06.js"
 );
 
 self.addEventListener('message', (event) => {
