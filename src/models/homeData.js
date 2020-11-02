974ee8829493a816bc77304f20fabc002aac27e5
sangmin802 <56113148+sangmin802@users.noreply.github.com>
2020-11-02 16:18:05 +0900

    img

--- src/models/homeData.js
+++ src/models/homeData.js
@@ -1,4 +1,5 @@
 import Factory from '../factory.js';
+import {calendarIsland} from '../JSON.js';
 
 export default class HomeData {
   constructor(raw){
@@ -31,25 +32,17 @@ export default class HomeData {
   setCalendar(raw){
     const 
       timerList = [...raw.getElementsByClassName('info')];
-      // npcicon
-      // gentime
-      // npcname
+
 
     this.calendar = timerList.reduce((prev, next) => {
       const
-        src = next.getElementsByClassName('npcicon')[0].attributes.src.value,
         name = next.getElementsByClassName('npcname')[0].textContent,
-        obj = {
-          src, 
-          name, 
-          lv : '-', 
-          position : '대항해', 
-        }
+        target = calendarIsland.find(cal => cal.name===name);
 
       if(next.textContent.includes('14:00')){
-        prev[0].push({...obj, time : ['14:13'], endTime : '14:13'});
+        prev[0].push({...target, time : ['14:13'], endTime : '14:13'});
       }else if(next.textContent.includes('21:00')){
-        prev[1].push({...obj, time : ['21:13'], endTime : '21:13'});
+        prev[1].push({...target, time : ['21:13'], endTime : '21:13'});
       }
       return prev;
     }, [[], []])
