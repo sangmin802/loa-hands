d2f6a7c73b597cd816b421d36418503c58972418
sangmin802 <56113148+sangmin802@users.noreply.github.com>
2020-11-04 12:18:03 +0900

    calendar, flwx-wrap

--- src/models/homeData.js
+++ src/models/homeData.js
@@ -31,19 +31,30 @@ export default class HomeData {
 
   setCalendar(raw){
     const 
-      timerList = [...raw.getElementsByClassName('info')];
+      timerList = [...raw.getElementsByClassName('info')],
+      _ = new Date(),
+      today = `${_.getFullYear()}-${this.zero(_.getMonth()+1)}-${this.zero(_.getDate())}`;
 
     this.calendar = timerList.reduce((prev, next) => {
       const
         name = next.getElementsByClassName('npcname')[0].textContent,
         target = calendarIsland.find(cal => cal.name===name);
 
-      if(next.textContent.includes('14:00') && target){
-        prev[0].push({...target, time : ['14:13'], endTime : '14:13'});
-      }else if(next.textContent.includes('21:00') && target){
-        prev[1].push({...target, time : ['21:13'], endTime : '21:13'});
+      if(next.textContent.includes(today) && target){
+        if(next.textContent.includes('14:00')){
+          prev[0].push({...target, time : ['14:13'], endTime : '14:13'});
+        }else if(next.textContent.includes('21:00')){
+          prev[1].push({...target, time : ['21:13'], endTime : '21:13'});
+        }
       }
       return prev;
     }, [[], []])
+  }
+
+  zero(num){
+    if(num < 10){
+      return '0'+num;
+    }
+    return num;
   }
 }
