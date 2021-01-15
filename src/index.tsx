8b7bf2b98256cf0f9565a46d6abfba0304ad6ff9
DESKTOP-LB3HQR1\USER <sangmin802>
2021-01-15 13:53:15 +0900

    테스트2

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,6 @@ import { Provider } from 'react-redux'
 import {store} from './store';
 import AppContainer from './components/AppContainer';
 
-// test
-
 ReactDOM.render(
   <Provider store = { store }>
     <BrowserRouter basename={process.env.PUBLIC_URL}>
