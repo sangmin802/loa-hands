f5a1b4689e867c8e20b7fb319792e90dcbb77506
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-03-05 17:33:46 +0900

    Updates

---
+++ precache-manifest.5f78c61a048bea4bb54043805bec7791.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86907aa28b1208871dfc866e27bd5219",
+    "url": "/loa-hands/index.html"
+  },
+  {
+    "revision": "4eec3750db9dafd77713",
+    "url": "/loa-hands/static/css/main.c6c39dd0.chunk.css"
+  },
+  {
+    "revision": "236fb6aaea13b5676387",
+    "url": "/loa-hands/static/js/2.b4bb7f5d.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/loa-hands/static/js/2.b4bb7f5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4eec3750db9dafd77713",
+    "url": "/loa-hands/static/js/main.c57d7d74.chunk.js"
+  },
+  {
+    "revision": "b895c641323837b7fd48",
+    "url": "/loa-hands/static/js/runtime-main.02ec9361.js"
+  }
+]);
