6151147eb391cd196f0e7a53314093bdbb36a7c1
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-03-15 17:33:55 +0900

    21-03-15/17:33 1. body.overflow-y hidden의 경우 ios에서는 작동되지 않아, position fixed, scroll To로 변경

--- src/hooks/dialogOpen.ts
+++ src/hooks/dialogOpen.ts
@@ -3,11 +3,22 @@ import { useEffect } from "react";
 const RemoveScrollHook = dialog => {
   useEffect(() => {
     const body = document.querySelector("body");
+    const top = window.pageYOffset;
     if (dialog) {
-      body.classList.add("dialogOpen");
+      const style = `
+        position: fixed; 
+        top: -${top}px;
+        left: 0px;
+        right: 0px;          
+      `;
+      body.setAttribute("style", style);
     }
     return () => {
-      body.classList.remove("dialogOpen");
+      // dialog가 꺼질 때
+      if (dialog) {
+        body.setAttribute("style", "");
+        window.scrollTo(0, top);
+      }
     };
   }, [dialog]);
 };
