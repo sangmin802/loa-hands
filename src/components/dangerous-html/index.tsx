32c590c683867a8c2e357edf31ec96824bc32488
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-06-04 18:00:14 +0900

    클래스명 추가 - dangerous-html 1. dangerous-html 클래스명 추가

--- src/components/dangerous-html/index.tsx
+++ src/components/dangerous-html/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const DangerousHTML = ({ html }) => (
-  <div dangerouslySetInnerHTML={{ __html: html }} />
+  <div className="dangerous-html" dangerouslySetInnerHTML={{ __html: html }} />
 );
 
 export default DangerousHTML;
