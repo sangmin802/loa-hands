e384d2ee13064d46fc05d68b4f665689b957c4f6
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-06-04 18:13:08 +0900

    테스트코드 작성 - collection 1. 계산을 통해 맞는 스타일과 값을 렌더링하는지 확인

---
+++ src/components/collection/index.test.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Collection from "./index";
+
+describe("Collection", () => {
+  it("Collection 컴포넌트 적절한 값 렌더링", () => {
+    // index 5 -> 627px
+    const { container } = render(<Collection size={80} index={5} />);
+    const backgroundStyle = container
+      .querySelector(".collection-mini-bg")
+      .getAttribute("style");
+    const result = "background-position: 627px 50px;";
+    expect(screen.getByText("80")).toBeInTheDocument();
+    expect(backgroundStyle).toBe(result);
+  });
+});
