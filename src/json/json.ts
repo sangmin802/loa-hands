270038974014b22f341e1d3a454ca0cee3c0cd9a
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-06-06 00:14:22 +0900

    json 수정 - json 1. 장비 백그라운드 이미지 순서 변경

--- src/json/json.ts
+++ src/json/json.ts
@@ -25,12 +25,12 @@ export const PARTS_ARR = [
 ];
 
 export const PARTS_IMAGE = [
-  "/2018/obt/assets/images/common/game/bg_equipment_slot1.png?847938d1e0f475ca73717df97334937d",
-  "/2018/obt/assets/images/common/game/bg_equipment_slot2.png?050db99e03db1afa7f32f00f53010a0a",
-  "/2018/obt/assets/images/common/game/bg_equipment_slot3.png?ad02ff0c0c7f0f750d7254629f61f433",
-  "/2018/obt/assets/images/common/game/bg_equipment_slot4.png?298c62509ce611528f532b8cd05c1de4",
-  "/2018/obt/assets/images/common/game/bg_equipment_slot5.png?206f2a75948642dbd0cddc37dd8ed220",
-  "/2018/obt/assets/images/common/game/bg_equipment_slot6.png?aac2a45f5db714c267e1c59e52dc4286",
+  "/2018/obt/assets/images/common/game/bg_equipment_slot6.png?847938d1e0f475ca73717df97334937d",
+  "/2018/obt/assets/images/common/game/bg_equipment_slot1.png?050db99e03db1afa7f32f00f53010a0a",
+  "/2018/obt/assets/images/common/game/bg_equipment_slot2.png?ad02ff0c0c7f0f750d7254629f61f433",
+  "/2018/obt/assets/images/common/game/bg_equipment_slot3.png?298c62509ce611528f532b8cd05c1de4",
+  "/2018/obt/assets/images/common/game/bg_equipment_slot4.png?206f2a75948642dbd0cddc37dd8ed220",
+  "/2018/obt/assets/images/common/game/bg_equipment_slot5.png?aac2a45f5db714c267e1c59e52dc4286",
   "/2018/obt/assets/images/common/game/bg_equipment_slot7.png?8fc19274483852c953d0d4d44ffed661",
   "/2018/obt/assets/images/common/game/bg_equipment_slot8.png?c3a33be4689986ee75e8a8432ee494d5",
   "/2018/obt/assets/images/common/game/bg_equipment_slot9.png?7326a4a95b92262ab12c1eb47656be34",
