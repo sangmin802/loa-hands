107b85d36c1d2dde26ec829f5135af6a7baa75fc
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-06-05 22:51:02 +0900

    이벤트매니저 추가 - interval 1. interval을 관리해주는 이벤트매니저 함수 인스턴스 추가

---
+++ src/utils/events/interval.ts
@@ -0,0 +1,16 @@
+export function interval(t, cb) {
+  const intervalArr = [];
+
+  function startInterval(args) {
+    cb(args);
+    const interval = setInterval(() => cb(args), t * 1000);
+    intervalArr.push(interval);
+    return interval;
+  }
+
+  function endInterval() {
+    intervalArr.forEach(interval => clearInterval(interval));
+  }
+
+  return { startInterval, endInterval };
+}
