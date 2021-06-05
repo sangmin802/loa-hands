b67059662b6e1f46ba164445445a865b52e61f99
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-06-05 22:53:49 +0900

    hook 추가 - useTimerType 1. 실제 타이머 훅에서 interval로 남은시간을 계산할 수 있도록, 현재시간과 목표시간간의 차이를 계산. 2. 목표시간이 없다면 null 반환 3. gap이 0이된다면 새로운 목표시간을 받아오도록 함 4. lost ark 특성상, 남은 시간 10분, 3분마다 알림이 다름을 구현

---
+++ src/hooks/use-timer-type.ts
@@ -0,0 +1,72 @@
+import { useState, useCallback, useMemo } from "react";
+
+export const useTimerType = (time, endTime, setTime) => {
+  const [timerType, setState] = useState("NORMAL");
+
+  const calcTimer = useCallback(
+    timeArr => {
+      const time = timeArr[0] ?? null;
+      if (!time) {
+        setState("END");
+        return null;
+      }
+      const now = new Date();
+      const [hour, min] = time.split(":");
+      const year = now.getFullYear();
+      const month = now.getMonth();
+      const date = now.getDate();
+      const closeTime = new Date(year, month, date, hour, min * 1 + 3); // 종료 시간 = 시작시간+3
+      const startTime = new Date(year, month, date, hour, min); // 시작 시간
+
+      let gap = startTime.getTime() - now.getTime();
+      let type = "NORMAL";
+
+      if (gap <= 600000) type = "READY";
+      if (gap === 0 || 0 > gap) {
+        type = "START";
+        gap = closeTime.getTime() - now.getTime();
+      }
+
+      if (gap === 0 || 0 > gap) {
+        setTime(time);
+        return null;
+      }
+      setState(type);
+      return gap;
+    },
+    [setState, setTime]
+  );
+
+  const timerVariables = useMemo(
+    () => ({
+      READY: {
+        borderColor: "#CC99FF",
+        endTimeBg: "none",
+        contentAlert: "컨텐츠 준비중",
+      },
+      START: {
+        borderColor: "#FF6666",
+        endTimeBg: "none",
+        contentAlert: "컨텐츠 시작",
+      },
+      NORMAL: {
+        borderColor: "inherit",
+        endTimeBg: "none",
+        contentAlert: time,
+      },
+      END: {
+        borderColor: "inherit",
+        endTimeBg: "block",
+        contentAlert: endTime,
+      },
+    }),
+    [time, endTime]
+  );
+
+  const timerVariable = useMemo(
+    () => timerVariables[timerType],
+    [timerType, timerVariables]
+  );
+
+  return { timerVariable, calcTimer };
+};
