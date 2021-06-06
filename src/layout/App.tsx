8c1b8ec77be7761d829095989e56a2c143d1b30a
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-06-06 15:16:56 +0900

    app 변경 1. loadingSpinner 간략화

--- src/layout/App.tsx
+++ src/layout/App.tsx
@@ -4,11 +4,9 @@ import { Route } from "react-router-dom";
 import { useLoadingToggle } from "hooks/use-loadingtoggle";
 import { useHome } from "hooks/use-home";
 import { useUser } from "hooks/use-user";
-import Header from "components/header/index";
+import { Header, LoadingSpinner } from "components/";
 import Home from "pages/home";
 import UserInfo from "pages/user-info";
-import LoadingSpinner from "components/loading-spinner/index";
-import Dialog from "components/dialog/index";
 
 function App() {
   const { isLoading } = useLoadingToggle();
@@ -17,10 +15,7 @@ function App() {
 
   return (
     <div className={`app ${isLoading ? "height100vh" : ""}`}>
-      <LoadingSpinner
-        style={isLoading ? "loading-bg z-index99" : "display-none"}
-      />
-      <Dialog />
+      {isLoading && <LoadingSpinner />}
       <div className="inner-app">
         <Header setHomeData={setHomeData} setUserData={setUserData} />
         <div className="app-wrap">
