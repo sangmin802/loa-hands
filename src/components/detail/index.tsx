19e1213adfaa3174cb482a5a3ba0c26b91c1fef2
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-06-08 18:56:12 +0900

    컴포넌트 수정 - detail 1. 해당 컴포넌트의 외부 구조에 영향을 주는 스타일속성들은 모두 상위 컴포넌트에서 부여하도록 함      - 색상, 선, padding 등은 컨텐츠 내부를 담당하기때문에 상관 x - expedition - header - list-item - loading-spinner - nav - quality - rune 위 컴포넌트들 작업 필요

--- src/components/detail/index.tsx
+++ src/components/detail/index.tsx
@@ -1,4 +1,4 @@
-import React, { cloneElement } from "react";
+import React, { cloneElement, useMemo } from "react";
 import Lodash from "lodash";
 import { ItemPartBox, IndentString, TripodSkillCustom, Image, Text } from "../";
 import * as Styled from "./index.style";
@@ -15,18 +15,23 @@ const Detail = ({ data, children }: { data?; children? }) => {
     tripodSkillCustom,
   } = detail;
 
+  const titleColor = useMemo(
+    () => (grade ? `color${grade}` : "white"),
+    [grade]
+  );
+
   return (
-    <div className="detail">
+    <>
       <Styled.Top>
         <Styled.Container>
-          <Image src={src ?? backSrc} grade={grade} />
+          <Image src={src ?? backSrc} color={`gradient${grade}`} />
           <Styled.Desc>
             {subTitle.map(res => (
               <Text type="subTitle" key={res}>
                 {res}
               </Text>
             ))}
-            <Text type="title" grade={grade}>
+            <Text type="title" color={titleColor}>
               {title}
             </Text>
           </Styled.Desc>
@@ -34,11 +39,11 @@ const Detail = ({ data, children }: { data?; children? }) => {
       </Styled.Top>
       {children && cloneElement(children, { data })}
       <>
-        <ItemPartBox data={itemPartBox} />
-        <IndentString data={indentStringGroup} />
-        <TripodSkillCustom data={tripodSkillCustom} />
+        {itemPartBox && <ItemPartBox data={itemPartBox} />}
+        {indentStringGroup && <IndentString data={indentStringGroup} />}
+        {tripodSkillCustom && <TripodSkillCustom data={tripodSkillCustom} />}
       </>
-    </div>
+    </>
   );
 };
 
