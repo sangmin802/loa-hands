226148592177e3b29c4dfd47d920a8ca201512d6
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-06-08 18:54:38 +0900

    컴포넌트 수정 - collection 1. Text 컴포넌트 사용

--- src/components/collection/index.tsx
+++ src/components/collection/index.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Lodash from "lodash";
 import * as Styled from "./index.style";
+import { Text } from "components/";
 
 const Collection = ({ size, index }) => {
   return (
     <Styled.Container>
       <Styled.Background position={index} />
-      <Styled.Size>{size}</Styled.Size>
+      <Styled.Size>
+        <Text>{size}</Text>
+      </Styled.Size>
     </Styled.Container>
   );
 };
