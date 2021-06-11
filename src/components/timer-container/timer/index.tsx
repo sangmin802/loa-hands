8d8cf0a1e0e820200c760bd9d36ba3183215f6bb
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-06-11 20:19:14 +0900

    테스트코드 1. position 속성의 타입별 변동값 2. 타임아웃 계산 로직 3. 타이머
    애로사항
    spyOn해놓은 모듈을 두가지 테스트에서 사용하는데,     jest.restoreAllMocks();를 해주지 않아서 Date 메소드가 두번째 테스트 때에 mock 인스턴스였. 따라서 생성하는 상수에 Date 인스턴스가 할당되는것이 아니라, mock인스턴스가 할당되어 Date의 메소드들이 존재하지 않는다는 에러가 발생함.
    spyOn 메소드를 사용했다면, 꼭  jest.restoreAllMocks()를 사용하여 해당 모듈을 원래의 상태로 돌려놓아야 함.

--- src/components/timer-container/timer/index.tsx
+++ src/components/timer-container/timer/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useTimer } from "hooks/use-timer";
 import { useTimerType } from "hooks/use-timer-type";
 import { Text, Image } from "components/";
@@ -17,7 +17,11 @@ const Timer = ({ setTime, data }) => {
   const timeOut = useTimer(time, calcTimer);
 
   return (
-    <Styled.Container borderColor={borderColor}>
+    <Styled.Container
+      role="timer-border"
+      data-border={borderColor}
+      borderColor={borderColor}
+    >
       <Styled.Background isShow={endTimeBg}></Styled.Background>
       <Styled.Title>
         <Text type="desc">{name}</Text>
@@ -30,13 +34,15 @@ const Timer = ({ setTime, data }) => {
           </Styled.Lv>
         </Styled.TimerImg>
         <Styled.Time>
-          <Text type="subTitle">{contentAlert}</Text>
-          <Text type="subTitle" color="timeOut">
+          <Text type="subTitle" role="timer-alert">
+            {contentAlert}
+          </Text>
+          <Text type="subTitle" role="timeout" color="timeOut">
             {timeOut}
           </Text>
         </Styled.Time>
       </Styled.Content>
-      <Styled.Position>
+      <Styled.Position role="position">
         <Text type="desc">{pos}</Text>
       </Styled.Position>
     </Styled.Container>
