e0c2a04f10e932b876a672a44bd6aa0c1ad4a1f0
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-06-15 17:22:49 +0900

    컴포넌트 수정 1. 기존의 header 컴포넌트 내용 app에 병합

--- src/layout/app/index.style.tsx
+++ src/layout/app/index.style.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { BACKGROUND } from "constants/index";
 
 export const Container = styled.div<{ isLoading: boolean }>`
   position: relative;
@@ -20,4 +21,46 @@ export const Main = styled.main`
 
 export const HeaderContainer = styled.div`
   margin-bottom: 1.5rem;
+`;
+
+export const InnerHeader = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+  padding: 0.5rem;
+`;
+
+export const Background = styled.div`
+  background-image: url("${BACKGROUND}");
+  background-position: 403px 3.5px;
+  width: 160px;
+  margin: 0 auto 10px;
+  height: 50px;
+  cursor: pointer;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  margin: 0 auto;
+  width: 60%;
+  height: fit-content;
+`;
+
+export const InputText = styled.div`
+  width: 80%;
+  padding: 0.3rem;
+  border-bottom: 1px solid #f0f4f5;
+
+  input {
+    font-size: 1rem;
+  }
+`;
+
+export const InputSubmit = styled.b`
+  width: 20%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  ${({ theme }) => theme.contentBox}
+  border-radius: 3px 3px 3px 0;
 `;
--- src/layout/app/index.tsx
+++ src/layout/app/index.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useRef, useCallback } from "react";
 import { Route } from "react-router-dom";
 import { useLoadingToggle } from "hooks/use-loadingtoggle";
 import { useHome } from "hooks/use-home";
 import { useUser } from "hooks/use-user";
-import { Header, LoadingSpinner } from "components/";
+import { LoadingSpinner, Input, Text, Button } from "components/";
 import { Home, UserInfo } from "../../pages/";
 import * as Styled from "./index.style";
 
@@ -11,13 +11,39 @@ function App() {
   const { isLoading } = useLoadingToggle();
   const { setHomeData } = useHome();
   const { setUserData } = useUser();
+  const textInput = useRef(null);
+  const onSubmitHandler = useCallback(
+    e => {
+      e.preventDefault();
+      setUserData(textInput.current.value);
+      textInput.current.value = null;
+    },
+    [textInput, setUserData]
+  );
 
   return (
     <Styled.Container isLoading={isLoading}>
-      {isLoading && <LoadingSpinner />}
+      {isLoading && <LoadingSpinner role="loading-spinner" />}
       <Styled.InnerContainer>
         <Styled.HeaderContainer>
-          <Header setHomeData={setHomeData} setUserData={setUserData} />
+          <Styled.InnerHeader>
+            <Styled.Background role="go-home" onClick={setHomeData} />
+            <Styled.Form role="form" onSubmit={onSubmitHandler}>
+              <Styled.InputText>
+                <Input
+                  type="text"
+                  name="searchedUser"
+                  ref={textInput}
+                  autoComplete="off"
+                />
+              </Styled.InputText>
+              <Styled.InputSubmit>
+                <Button>
+                  <Text>검색</Text>
+                </Button>
+              </Styled.InputSubmit>
+            </Styled.Form>
+          </Styled.InnerHeader>
         </Styled.HeaderContainer>
         <Styled.Main>
           <Route exact path="/" component={Home} />
