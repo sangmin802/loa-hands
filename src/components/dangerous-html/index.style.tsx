0c68d12f64a7f3624414b44ee0cf13ab590856dc
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-06-18 17:58:32 +0900

    stories 추가
    1. dangeroushtml을 통해 문자열 돔을 변환하는 과정에서 커스텀 글씨크기가 잘 지정되는지 확인

--- src/components/dangerous-html/index.style.tsx
+++ src/components/dangerous-html/index.style.tsx
@@ -3,4 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div`
   color: ${({ theme }) => theme.fontColor["white"]};
   font-size: ${({ theme }) => theme.fontSize["desc"]};
+  * {
+    font-size: inherit;
+  }
 `;
