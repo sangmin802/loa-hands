04202e4fab950c0af7244b7014a184f503c01de1
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-07-02 16:43:32 +0900

    hooks 업데이트 - homeData를 받는 비동기 과정을 Redux, Redux-Saga 에서 React Query 단일로 변경 - ContextApi를 내재하고 있어서, Redux와 비슷한 기능도 하여캐싱기능도 있다고 함 - Suspense 기능을 생각한다면 React Query가 더 적합할 듯

--- src/hooks/use-home.ts
+++ src/hooks/use-home.ts
@@ -1,23 +1,37 @@
-import { useCallback } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useHistory } from "react-router";
-import { getHomeData_Saga_Async } from "store/ducks/ajax-slicer";
-import { RootState } from "store/index";
-import Lodash from "lodash";
+// import { useCallback, useEffect } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { useHistory } from "react-router";
+// import { getHomeData_Saga_Async } from "store/ducks/ajax-slicer";
+// import { getHomeData } from "store/ducks/ajax-slicer/index";
+// import { RootState } from "store/index";
+// import Lodash from "lodash";
+
+// export function useHome() {
+//   const dispatch = useDispatch();
+//   const history = useHistory();
+
+//   const homeData = useSelector(
+//     (state: RootState) => state.ajaxReducer.homeData,
+//     (left, right) => Lodash.isEqual(left, right)
+//   );
+
+//   const setHomeData = useCallback(() => {
+//     history.replace("/");
+//     dispatch(getHomeData_Saga_Async(history));
+//   }, [dispatch, history]);
+
+//   return { homeData, setHomeData };
+// }
+
+import API from "api/api";
+import { useQuery } from "react-query";
 
 export function useHome() {
-  const dispatch = useDispatch();
-  const history = useHistory();
-
-  const homeData = useSelector(
-    (state: RootState) => state.ajaxReducer.homeData,
-    (left, right) => Lodash.isEqual(left, right)
+  const { data: HomeData } = useQuery(
+    "fetchHomeData",
+    () => API.getHomeData(),
+    { suspense: true }
   );
 
-  const setHomeData = useCallback(() => {
-    history.replace("/");
-    dispatch(getHomeData_Saga_Async(history));
-  }, [dispatch, history]);
-
-  return { homeData, setHomeData };
+  return HomeData;
 }
