495b8939bab2369849f7348f719174f63dd6a826
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-07-02 16:49:05 +0900

    suspense 브랜치 체리픽 - 컴포넌트 업데이트 1. setHomeData가 아닌 setNow를 통해 자정에 오늘 날짜를 바꾸도록 함

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,10 @@
-import React, { useCallback, useEffect, useMemo } from "react";
+import React, {
+  useCallback,
+  useEffect,
+  useMemo,
+  Suspense,
+  useState,
+} from "react";
 import { DAILY_ISLAND, FIELD_BOSS, CHAOS_GATE, OCEAN_ACT } from "json/json";
 import { useHome } from "hooks/use-home";
 import {
@@ -11,18 +17,17 @@ import { interval } from "utils/events/interval";
 import * as Styled from "./index.style";
 
 const Home = () => {
-  const { homeData, setHomeData } = useHome();
-  const now = new Date();
+  const [now, setNow] = useState(new Date());
   const today = now.getDate();
   const yoil = now.getDay();
 
-  const checkNight = useCallback(setHomeData => {
+  const checkNight = useCallback(setNow => {
     const now = new Date();
     const hour = now.getHours();
     const min = now.getMinutes();
     const sec = now.getSeconds();
-    if (hour === 0 && min === 0 && sec === 0) {
-      setHomeData();
+    if (hour === 0 && min === 0 && sec === 1) {
+      setNow(now);
     }
   }, []);
 
@@ -32,57 +37,17 @@ const Home = () => {
   );
 
   useEffect(() => {
-    startInterval(setHomeData);
+    startInterval(setNow);
     return () => {
       endInterval();
     };
-  }, [endInterval, startInterval, setHomeData]);
-
-  useEffect(() => {
-    if (!homeData) setHomeData();
-  }, [homeData, setHomeData]);
+  }, [endInterval, startInterval, setNow]);
 
   return (
-    <section role="home" data-isdata={homeData ? true : false}>
-      <Styled.Section>
-        <SectionContainer title="진행중인 이벤트">
-          {!homeData?.events && <LoadingSpinner back={false} />}
-          {homeData?.events && (
-            <Styled.Content type="event">
-              {homeData?.events.map((event, index) => (
-                <Styled.Event key={`event${index}`}>
-                  <Event event={event} />
-                </Styled.Event>
-              ))}
-            </Styled.Content>
-          )}
-        </SectionContainer>
-      </Styled.Section>
-      <Styled.Section>
-        <SectionContainer title="오늘의 캘린더섬">
-          {!homeData?.calendar && <LoadingSpinner back={false} />}
-          {homeData?.calendar && (
-            <>
-              <Styled.Section>
-                <SectionContainer title="14:00">
-                  <TimerContainer
-                    data={homeData?.calendar[0] ?? []}
-                    today={today}
-                  />
-                </SectionContainer>
-              </Styled.Section>
-              <Styled.Section>
-                <SectionContainer title="21:00">
-                  <TimerContainer
-                    data={homeData?.calendar[1] ?? []}
-                    today={today}
-                  />
-                </SectionContainer>
-              </Styled.Section>
-            </>
-          )}
-        </SectionContainer>
-      </Styled.Section>
+    <>
+      <Suspense fallback={<LoadingSpinner back={false} />}>
+        <FetchHome today={today} />
+      </Suspense>
       <Styled.Section>
         <SectionContainer title="오늘의 모험섬">
           <TimerContainer data={DAILY_ISLAND} today={today} />
@@ -101,6 +66,46 @@ const Home = () => {
       <Styled.Section>
         <SectionContainer title="오늘의 항해">
           <TimerContainer data={OCEAN_ACT[yoil]} today={today} />
+        </SectionContainer>
+      </Styled.Section>
+    </>
+  );
+};
+
+const FetchHome = ({ today }) => {
+  const HomeData = useHome();
+
+  return (
+    <section role="home" data-isdata={HomeData ? true : false}>
+      <Styled.Section>
+        <SectionContainer title="진행중인 이벤트">
+          <Styled.Content type="event">
+            {HomeData?.events.map((event, index) => (
+              <Styled.Event key={`event${index}`}>
+                <Event event={event} />
+              </Styled.Event>
+            ))}
+          </Styled.Content>
+        </SectionContainer>
+      </Styled.Section>
+      <Styled.Section>
+        <SectionContainer title="오늘의 캘린더섬">
+          <Styled.Section>
+            <SectionContainer title="14:00">
+              <TimerContainer
+                data={HomeData?.calendar[0] ?? []}
+                today={today}
+              />
+            </SectionContainer>
+          </Styled.Section>
+          <Styled.Section>
+            <SectionContainer title="21:00">
+              <TimerContainer
+                data={HomeData?.calendar[1] ?? []}
+                today={today}
+              />
+            </SectionContainer>
+          </Styled.Section>
         </SectionContainer>
       </Styled.Section>
     </section>
