0cc4483a74b80642df9a2619abf38095c7faf162
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-07-03 18:17:24 +0900

    Header 컴포넌트 분리

--- src/layout/app/index.tsx
+++ src/layout/app/index.tsx
@@ -1,64 +1,22 @@
-import React, { useRef, useCallback, useState, useEffect } from "react";
-import { Route, Link } from "react-router-dom";
-import { useLoadingToggle } from "hooks/use-loadingtoggle";
-import { useUser } from "hooks/use-user";
-import { LoadingSpinner, Input, Text, Button } from "components/";
+import React, { Suspense } from "react";
+import { Route } from "react-router-dom";
+import { SearchLoading, Header } from "components/";
 import { Home, UserInfo } from "../../pages/";
 import * as Styled from "./index.style";
 
 function App() {
-  const [disabled, setDisabled] = useState(false);
-  const { isLoading } = useLoadingToggle();
-  const { setUserData } = useUser();
-  const textInput = useRef(null);
-
-  const onSubmitHandler = useCallback(
-    e => {
-      e.preventDefault();
-      setDisabled(true);
-      setUserData(textInput.current.value);
-      textInput.current.value = null;
-    },
-    [textInput, setUserData, setDisabled]
-  );
-
-  useEffect(() => {
-    return () => {
-      if (isLoading) setDisabled(false);
-    };
-  }, [isLoading, setDisabled]);
-
   return (
-    <Styled.Container isLoading={isLoading}>
-      {isLoading && <LoadingSpinner role="loading-spinner" />}
+    <Styled.Container isLoading={false}>
       <Styled.InnerContainer>
         <Styled.HeaderContainer>
-          <Styled.InnerHeader>
-            <Link to="/">
-              <Styled.Background role="go-home" />
-            </Link>
-            <Styled.Form role="form" onSubmit={onSubmitHandler}>
-              <Styled.InputText>
-                <Input
-                  type="text"
-                  name="searchedUser"
-                  ref={textInput}
-                  autoComplete="off"
-                  disabled={disabled}
-                />
-              </Styled.InputText>
-              <Styled.InputSubmit>
-                <Button>
-                  <Text>검색</Text>
-                </Button>
-              </Styled.InputSubmit>
-            </Styled.Form>
-          </Styled.InnerHeader>
+          <Header />
         </Styled.HeaderContainer>
-        <Styled.Main>
-          <Route exact path="/" component={Home} />
-          <Route path="/userInfo/:name" component={UserInfo} />
-        </Styled.Main>
+        <Suspense fallback={<SearchLoading />}>
+          <Styled.Main>
+            <Route exact path="/" component={Home} />
+            <Route path="/userInfo/:name" component={UserInfo} />
+          </Styled.Main>
+        </Suspense>
       </Styled.InnerContainer>
     </Styled.Container>
   );
