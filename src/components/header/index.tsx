5daf064a4de61b664e99052788cc12dd6f4ef15d
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-07-03 18:26:18 +0900

    컴포넌트 추가 - Header

---
+++ src/components/header/index.tsx
@@ -0,0 +1,46 @@
+import React, { useRef, useCallback } from "react";
+import { Link, useHistory } from "react-router-dom";
+import * as Styled from "./index.style";
+import { Input, Button, Text } from "components/";
+
+const Header = () => {
+  const textInput = useRef(null);
+  const history = useHistory();
+
+  const onSubmitHandler = useCallback(
+    e => {
+      e.preventDefault();
+
+      const name = textInput?.current?.value;
+      history.replace(`/userInfo/${name}`);
+      textInput.current.value = null;
+    },
+    [textInput, history]
+  );
+
+  return (
+    <Styled.Container>
+      <Link to="/">
+        <Styled.Background role="go-home" />
+      </Link>
+      <Styled.Form role="form" onSubmit={onSubmitHandler}>
+        <Styled.InputText>
+          <Input
+            className="search-area"
+            type="text"
+            name="searchedUser"
+            ref={textInput}
+            autoComplete="off"
+          />
+        </Styled.InputText>
+        <Styled.InputSubmit>
+          <Button>
+            <Text>검색</Text>
+          </Button>
+        </Styled.InputSubmit>
+      </Styled.Form>
+    </Styled.Container>
+  );
+};
+
+export default Header;
