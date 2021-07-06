24af0b520550cfd9a4b9f53c9b90601a33059fc6
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-07-06 15:45:50 +0900

    basicInfo update

--- src/components/basic-info/index.styles.tsx
+++ src/components/basic-info/index.styles.tsx
@@ -2,7 +2,7 @@ import styled, { css } from "styled-components";
 
 const contentType = {
   title: css`
-    margin-right: 0.7rem;
+    margin-right: 0.3rem;
     padding: 0.2rem 0.6rem;
     overflow: initial;
 
