7e0d5e3037cc3f3b842757e8da85fe9688d6ddce
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-07-06 16:44:57 +0900

    errorFallback component - async 작업중, 에러 발생 시 해당 컴포넌트 출력 - 상황에 따라 다른 레이아웃을 사용할 수 있음

---
+++ src/components/error-fallback/index.style.tsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const ImageContainer = styled.div`
+  width: 40%;
+  max-width: 300px;
+  margin: 20px auto 40px;
+  border-radius: 50%;
+  overflow: hidden;
+`;
+
+export const TextContainer = styled.div`
+  text-align: center;
+`;
+
+export const ButtonContainer = styled.div`
+  width: fit-content;
+  padding: 0.3rem 0.6rem;
+  margin: 10px auto 0;
+  ${({ theme }) => theme.contentBox}
+`;
