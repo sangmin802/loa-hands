2d0490a00caa596213d1ccb168ecfa7469a64008
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-07-08 15:58:55 +0900

    text props들 타입 지정

--- src/components/text/index.tsx
+++ src/components/text/index.tsx
@@ -1,13 +1,13 @@
-import React, { HTMLAttributes, ReactNode } from "react";
+import React, { HTMLAttributes, PropsWithChildren, ReactNode } from "react";
 import * as Styled from "./index.style";
 
-export interface Props extends HTMLAttributes<HTMLDivElement> {
+export interface IText extends HTMLAttributes<HTMLDivElement> {
   children: ReactNode;
   type?: string;
   color?: string;
 }
 
-const Text = ({ children, ...props }: Props) => (
+const Text = ({ children, ...props }: PropsWithChildren<IText>) => (
   <Styled.Text {...props}>{children}</Styled.Text>
 );
 
