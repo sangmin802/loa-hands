0af1e0105ebe2260fbee960d3aff5cf73fd68280
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-07-08 15:04:14 +0900

    Image props type 설정

--- src/components/image/index.tsx
+++ src/components/image/index.tsx
@@ -1,11 +1,11 @@
-import React, { ImgHTMLAttributes } from "react";
+import React, { ImgHTMLAttributes, PropsWithChildren } from "react";
 import * as Styled from "./index.style";
 
 interface Props extends ImgHTMLAttributes<HTMLImageElement> {
   color: string;
 }
 
-const Image = ({ color, ...props }: Partial<Props>) => {
+const Image = ({ color, ...props }: PropsWithChildren<Partial<Props>>) => {
   return (
     <div className="img-container">
       <Styled.Image color={color} {...props} />
