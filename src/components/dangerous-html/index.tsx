3b51d1b53e26786ab3763f6782eaad0e885b97db
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-07-08 15:04:33 +0900

    dangerousHtml props type 설정

--- src/components/dangerous-html/index.tsx
+++ src/components/dangerous-html/index.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { PropsWithChildren } from "react";
 import * as Styled from "./index.style";
 
-const DangerousHTML = ({ html }) => {
+interface IDangerousHTML {
+  html: string;
+}
+
+const DangerousHTML = ({ html }: PropsWithChildren<IDangerousHTML>) => {
   return <Styled.Container dangerouslySetInnerHTML={{ __html: html }} />;
 };
 
