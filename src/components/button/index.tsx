29f8f35851bfe0b9a76c824fcb9fbb77ebe1594d
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-07-08 15:05:02 +0900

    button props type 설정

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,11 +1,15 @@
-import React, { ButtonHTMLAttributes, ReactNode } from "react";
+import React, {
+  ButtonHTMLAttributes,
+  PropsWithChildren,
+  ReactNode,
+} from "react";
 import * as Styled from "./index.style";
 
-export interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
+export interface IButton extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
 }
 
-const Button = ({ children, ...props }: Props) => (
+const Button = ({ children, ...props }: PropsWithChildren<IButton>) => (
   <Styled.Button {...props}>{children}</Styled.Button>
 );
 
