a88682f5f152ff9e2920e3228d29c8cc93f52cfe
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-07-09 19:26:40 +0900

    user정보를 받는 비동기로직과 관련없는 로직은 밖으로 배출

--- src/hooks/use-user.ts
+++ src/hooks/use-user.ts
@@ -20,25 +20,5 @@ export function useUser(name) {
     }
   );
 
-  const infos = useMemo(() => {
-    const {
-      characteristicInfo,
-      equipInfo: { equipment, avatar },
-    } = userData.abilityInfo;
-    const { battleSkill = null, lifeSkill } = userData.skillInfo;
-    const { collectionDetail } = userData.collectionInfo;
-    const { battle, basic, engrave } = characteristicInfo;
-    return {
-      equipment,
-      avatar,
-      battleSkill,
-      lifeSkill,
-      collectionDetail,
-      battle,
-      basic,
-      engrave,
-    };
-  }, [userData]);
-
-  return { userData, infos };
+  return userData;
 }
