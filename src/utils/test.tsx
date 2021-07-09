66fda6aa3ae77983a77d06be598134e884bdaade
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-07-09 19:23:51 +0900

    react-query 테스트를 위한 provider 적용

--- src/utils/test.tsx
+++ src/utils/test.tsx
@@ -4,13 +4,32 @@ import { Provider } from "react-redux";
 import { store } from "store/index";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
+import { QueryClientProvider, QueryClient } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 import { THEME } from "global-style";
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      suspense: true,
+      useErrorBoundary: true,
+      cacheTime: 1000 * 60 * 5,
+      staleTime: 1000 * 60 * 3,
+    },
+  },
+});
 
 const AllProviders = ({ children }) => {
   return (
     <Provider store={store}>
       <BrowserRouter basename={process.env.PUBLIC_URL}>
-        <ThemeProvider theme={THEME}>{children}</ThemeProvider>
+        <ThemeProvider theme={THEME}>
+          <QueryClientProvider client={queryClient}>
+            <ReactQueryDevtools />
+            {children}
+          </QueryClientProvider>
+        </ThemeProvider>
       </BrowserRouter>
     </Provider>
   );
