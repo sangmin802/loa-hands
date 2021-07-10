0fa6b9c5fef54ed20ebae839d8e4606c5bf79814
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-07-10 15:59:19 +0900

    app test 1. route 이동관련 테스트 수정 2. asyncboundary에서 로딩, 에러, 재시도 테스트해보기

--- src/layout/app/index.test.tsx
+++ src/layout/app/index.test.tsx
@@ -1,59 +1,48 @@
-import { fireEvent, render, screen, waitFor } from "utils/test";
+import { fireEvent, render, waitFor } from "utils/test";
+import { getUserData } from "api/api";
+import { USER_DATA } from "constants/index";
+
 import App from "./index";
 
+window.alert = jest.fn();
+jest.mock("api/api", () => ({
+  getUserData: jest.fn(),
+}));
+
 describe("App", () => {
-  describe("Route", () => {
-    beforeEach(() => {
-      jest.spyOn(global, "alert").mockImplementation(null);
+  describe("UserInfo 활성화", () => {
+    it("UserInfo Route 이동, 패치완료", async () => {
+      (getUserData as jest.Mock).mockResolvedValue(USER_DATA);
+
+      const { getByTestId, container } = render(<App />);
+
+      (container.querySelector(".search-area") as HTMLInputElement).value =
+        "모여요꿈동산";
+
+      fireEvent.submit(getByTestId("submit-form"));
+
+      await waitFor(() => expect(getByTestId("모여요꿈동산")).toBeTruthy());
     });
 
-    afterEach(() => {
-      jest.restoreAllMocks();
-    });
+    // describe('asyncBoundary', async() => {
+    //   it('로딩', () => {
+    //     (getUserData as jest.Mock).mockResolvedValue(USER_DATA);
 
-    it("유저 검색 실패시, alert 발생 후 / route 유지", async () => {
-      const { container } = render(<App />);
-      const input = container.querySelector("input");
-      input.value = "ㄹㄹㄹㄹㄹ";
+    //     const { getByTestId, container } = render(<App />);
 
-      fireEvent.click(screen.getByRole("button"));
+    //     (container.querySelector(".search-area") as HTMLInputElement).value =
+    //       "모여요꿈동산";
 
-      expect(screen.queryByRole("loading-spinner")).not.toBe(null);
+    //     await waitFor(() => expect(getByTestId("모여요꿈동산")).toBeTruthy());
+    //   })
 
-      await waitFor(() => expect(global.alert as jest.Mock).toBeCalledTimes(1));
+    //   it('에러', () => {
 
-      expect(input.value).toBe("");
-      expect(screen.queryByRole("home")).not.toBe(null);
-      expect(screen.queryByRole("loading-spinner")).toBe(null);
-    });
+    //   })
 
-    it("유저 검색 시 UserInfo 컴포넌트 출력 및, /userInfo/:name route로 이동", async () => {
-      const { container } = render(<App />);
-      const input = container.querySelector("input");
-      input.value = "모여요꿈동산";
+    //   it('재시도', () => {
 
-      fireEvent.click(screen.getByRole("button"));
-
-      expect(screen.queryByRole("loading-spinner")).not.toBe(null);
-
-      await waitFor(() => {
-        expect(screen.queryByRole("home")).toBe(null);
-        expect(screen.queryByRole("user-info")).not.toBe(null);
-      });
-
-      expect(input.value).toBe("");
-      expect(screen.queryByRole("loading-spinner")).toBe(null);
-    });
-
-    it("로고 클릭 시 Home 컴포넌트 출력 및, / route로 이동", async () => {
-      render(<App />);
-
-      fireEvent.click(screen.getByRole("go-home"));
-
-      await waitFor(() => {
-        expect(screen.queryByRole("home")).not.toBe(null);
-        expect(screen.queryByRole("user-info")).toBe(null);
-      });
-    });
+    //   })
+    // })
   });
 });
