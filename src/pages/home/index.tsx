f15d50a70e4dc63f4da856558252515b7848c6d7
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-07-12 20:38:15 +0900

    calendar 타이틀 시간대 변경 - 게임 업데이트함

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -88,6 +88,10 @@ const Home = () => {
 
 const FetchCalendar = ({ today }) => {
   const calendarData = useCalendar();
+  const [title1, title2] = useMemo(() => {
+    if (6 > yoil && yoil > 0) return ["11:00 ~ 21:00", null];
+    return ["09:00 ~ 13:00", "19:00 ~ 23:00"];
+  }, [yoil]);
 
   return (
     <>
