78edf759b4d7dfa0dca0c988f2653103b24c5979
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-07-22 14:00:09 +0900

    이벤트 위임(delegation)을 통한 불필요 속성 및 이벤트 핸들러 제거

--- src/components/expedition/char/index.tsx
+++ src/components/expedition/char/index.tsx
@@ -1,4 +1,4 @@
-import React, { PropsWithChildren, useCallback } from "react";
+import React, { PropsWithChildren } from "react";
 import { Button, Text } from "components/";
 import * as Styled from "./index.style";
 
@@ -8,14 +8,8 @@ interface IData {
 }
 
 interface IChar<T> {
-  setUserData: (T: string) => void;
   char: T;
 }
-
-const Char = ({ setUserData, char }: PropsWithChildren<IChar<IData>>) => {
-  const setUserDataEvent = useCallback(() => {
-    setUserData(char.name);
-  }, [char.name, setUserData]);
 
   return (
     <Styled.Container>
