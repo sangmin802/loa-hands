593298b9deae4bfd6910d48dbaf677871aff80c1
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-07-30 17:51:38 +0900

    cleancode 1. map 순회 추상회를 위한 컴포넌트 추가

---
+++ src/components/map-container/index.tsx
@@ -0,0 +1,19 @@
+import { cloneElement, PropsWithChildren, ReactElement } from "react";
+
+interface IMapContainer {
+  data: any[];
+  dataKey?: string;
+  children: ReactElement;
+}
+
+const MapContainer = ({
+  data = [],
+  dataKey = "data",
+  children,
+}: PropsWithChildren<IMapContainer>) => (
+  <>
+    {data.map((data, i) => cloneElement(children, { [dataKey]: data, key: i }))}
+  </>
+);
+
+export default MapContainer;
