bd4ac517af9ba26126bb9ddbd9a708fc0446c517
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-07-30 17:54:36 +0900

    cleancode 1. 이벤트 핸들러 네이밍 통일화 2. 하는 역할에 따른 함수 분리 3. ConditionalContainer 사용을 통한 조건부 렌더링에 대한 구문 모두 제거

--- src/components/dialog/index.tsx
+++ src/components/dialog/index.tsx
@@ -12,44 +12,43 @@ interface IDialog {
 }
 
 const Dialog = ({ dialog, setDialog }: PropsWithChildren<IDialog>) => {
-  const closeDialog = useCallback(() => {
+  const handleCloseDialog = useCallback(() => {
     setDialog(null);
   }, [setDialog]);
 
+  const fixViewport = useCallback((body, top) => {
+    const style = `
+    position: fixed;
+    top: -${top}px;
+    left: 0px;
+    right: 0px;
+  `;
+    body.setAttribute("style", style);
+  }, []);
+
+  const resetViewport = useCallback((body, top) => {
+    body.setAttribute("style", "");
+    window.scrollTo(0, top);
+  }, []);
+
   useEffect(() => {
-    // body에 overflow-y hidden 주는방식은 ios에서 적용 안됨
     const body = document.querySelector("body");
     const top = window.pageYOffset;
-    if (dialog) {
-      const style = `
-        position: fixed;
-        top: -${top}px;
-        left: 0px;
-        right: 0px;
-      `;
-      body.setAttribute("style", style);
-    }
+
+    fixViewport(body, top);
+
     return () => {
-      if (dialog) {
-        body.setAttribute("style", "");
-        window.scrollTo(0, top);
-      }
+      resetViewport(body, top);
     };
-  }, [dialog]);
+  }, [dialog, fixViewport, resetViewport]);
 
   return (
     <>
-      {dialog && (
-        <>
-          <Styled.Background
-            onClick={closeDialog}
-            data-testid="close-dialog"
-          ></Styled.Background>
-          <Styled.Container data-testid="dialog-content">
-            {dialog}
-          </Styled.Container>
-        </>
-      )}
+      <Styled.Background
+        onClick={handleCloseDialog}
+        data-testid="close-dialog"
+      />
+      <Styled.Container data-testid="dialog-content">{dialog}</Styled.Container>
     </>
   );
 };
