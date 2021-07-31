c6f43b41dd503727f9c2c2aa49e69d23b89acc59
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-07-31 13:18:05 +0900

    클린코드 1. TimerContainer 스타일 변경

--- src/components/timer-container/index.style.tsx
+++ src/components/timer-container/index.style.tsx
@@ -7,14 +7,14 @@ export const Alert = styled.article`
 export const Container = styled.section`
   display: flex;
   flex-wrap: wrap;
-`;
 
-export const Content = styled.article`
-  width: calc((100% - 4%) / 3 - 0.01px);
-  margin-right: 2%;
-  margin-bottom: 2%;
+  & > article {
+    width: calc((100% - 4%) / 3 - 0.01px);
+    margin-right: 2%;
+    margin-bottom: 2%;
 
-  &:nth-child(3n) {
-    margin-right: 0;
+    &:nth-child(3n) {
+      margin-right: 0;
+    }
   }
 `;
