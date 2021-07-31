9d89af88f85412848b4d5a9b35eb513cfe86d938
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-07-31 13:13:39 +0900

    클린코드 1. list-container 동적 생성 로직 mapContainer로 변경

--- src/components/list-container/index.tsx
+++ src/components/list-container/index.tsx
@@ -1,5 +1,5 @@
 import React, { cloneElement, PropsWithChildren, ReactElement } from "react";
-import { Text } from "components/";
+import { Text, MapContainer } from "components/";
 import * as Styled from "./index.style";
 
 interface IListContainer {
@@ -18,13 +18,9 @@ const ListContainer = ({
       <Styled.Title>
         <Text>{title}</Text>
       </Styled.Title>
-      <>
-        {arr.map((res, index) => (
-          <Styled.Content key={`item${index}`}>
-            {cloneElement(children, { data: res })}
-          </Styled.Content>
-        ))}
-      </>
+      <Styled.Content>
+        <MapContainer data={arr}>{cloneElement(children)}</MapContainer>
+      </Styled.Content>
     </>
   );
 };
