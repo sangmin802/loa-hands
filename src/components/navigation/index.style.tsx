49574a23b491999f326ec388907fdeb7dd2b1e35
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-08-05 15:25:46 +0900

    리팩토링 1. navigation 세부 스타일은 상위 컴포넌트에서 지정해주로고 함 2. navigation 내부 스타일은 고유한 속성만 갖도록 변경

--- src/components/navigation/index.style.tsx
+++ src/components/navigation/index.style.tsx
@@ -1,42 +1,12 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 
-const button = css`
+export const Container = styled.article`
+  padding: 0.5rem;
+  width: fit-content;
+  background: ${({ theme }) => theme.backgroundColor.darkDeep};
   > button {
     width: fit-content;
     margin-right: 1rem;
     color: #666;
   }
-`;
-
-const containerType = {
-  main: css`
-    border-radius: 5px 5px 0 0;
-
-    ${button}
-  `,
-
-  sub: css<{ isShow: boolean }>`
-    width: 100%;
-    border-radius: 0 5px 0 0;
-
-    ${button}
-    ${({ theme }) => theme.isShow}
-
-    &:nth-of-type(4) {
-      ${({ theme }) => theme.isFlex}
-
-      & > button {
-        flex-grow: 1;
-        margin-right: 0;
-      }
-    }
-  `,
-};
-
-export const Container = styled.article<{ isShow: boolean; type: string }>`
-  padding: 0.5rem;
-  width: fit-content;
-
-  background: ${({ theme }) => theme.backgroundColor.darkDeep};
-  ${({ type }) => containerType[type]}
 `;
--- src/pages/user-info/index.style.tsx
+++ src/pages/user-info/index.style.tsx
@@ -1,4 +1,20 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import { BACKGROUND } from "constants/index";
+
+const positionX = [
+  "718px",
+  "673px",
+  "767px",
+  "787px",
+  "652px",
+  "627px",
+  "741px",
+  "696px",
+];
+
+export const UserInfo = styled.section`
+  padding: 0.5rem;
+`;
 
 export const Top = styled.section`
   position: relative;
@@ -25,4 +41,73 @@ export const Container = styled.section`
   border-radius: 0 0 5px 5px;
 
   background: ${({ theme }) => theme.backgroundColor.darkDeep};
+`;
+
+const navigationType = {
+  main: css`
+    & > article {
+      border-radius: 5px 5px 0 0;
+    }
+  `,
+  sub: css<{ isShow: number; selected: number }>`
+    & > article {
+      width: 100%;
+      border-radius: 0 5px 0 0;
+
+      &:nth-of-type(3) {
+        & > button {
+          flex-grow: 1;
+          margin-right: 0;
+          border-radius: 3px;
+
+          &:hover {
+            background-color: #222;
+          }
+
+          ${({ selected }) =>
+            `&:nth-of-type(${selected + 1}){background-color : #222;}`}
+
+          & > div {
+            width: 20px;
+            height: 24px;
+            margin: 0 auto;
+            background: url("${BACKGROUND}");
+            background-size: 1250px 1209px;
+          }
+
+          ${() => {
+            let styles = "";
+            positionX.forEach(
+              (p, i) =>
+                (styles += `&:nth-of-type(${
+                  i + 1
+                })  > div {background-position: ${p} 50px;};`)
+            );
+            return css`
+              ${styles}
+            `;
+          }}
+        }
+      }
+    }
+
+    ${({ isShow }) =>
+      css`
+        & > article {
+          display: none;
+
+          &:nth-of-type(${isShow + 1}) {
+            display: ${isShow === 2 ? "flex" : "block"};
+          }
+        }
+      `}
+  `,
+};
+
+export const Navigation = styled.section<{
+  type: string;
+  isShow?: number;
+  selected?: number;
+}>`
+  ${({ type }) => navigationType[type]}
 `;
