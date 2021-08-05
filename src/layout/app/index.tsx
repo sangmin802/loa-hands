d86123f37f6123c20863d2e19566baa01395a96b
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-08-05 15:14:44 +0900

    collection-query 분리 1. Asyncboundary(errorboundary)를 UserInfo 내부로 옮김 2. Header컴포넌트 app 에 병합

--- src/layout/app/index.tsx
+++ src/layout/app/index.tsx
@@ -1,40 +1,20 @@
-import React, { cloneElement } from "react";
+import React from "react";
 import { Route } from "react-router-dom";
-import {
-  SearchLoading,
-  Header,
-  AsyncBoundary,
-  ErrorFallback,
-} from "components/";
+import { Header } from "components/";
 import { Home, UserInfo } from "../../pages/";
 import * as Styled from "./index.style";
 
 function App() {
   return (
     <Styled.Container>
-      <AsyncBoundary
-        suspenseFallback={<SearchLoading />}
-        errorFallback={<HeaderLayout children={<ErrorFallback />} />}
-      >
-        <HeaderLayout>
-          <Styled.Main>
-            <Route exact path="/" component={Home} />
-            <Route path="/userInfo/:name" component={UserInfo} />
-          </Styled.Main>
-        </HeaderLayout>
-      </AsyncBoundary>
-    </Styled.Container>
-  );
-}
-
-export function HeaderLayout({ children, ...props }) {
-  return (
-    <Styled.InnerContainer>
       <Styled.HeaderContainer>
-        <Header {...props} />
+        <Header />
       </Styled.HeaderContainer>
-      {cloneElement(children, { ...props })}
-    </Styled.InnerContainer>
+      <Styled.Main>
+        <Route exact path="/" component={Home} />
+        <Route path="/userInfo/:name" component={UserInfo} />
+      </Styled.Main>
+    </Styled.Container>
   );
 }
 
