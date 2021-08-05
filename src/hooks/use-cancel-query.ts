9934c113b6450885a56ccdfc4a3b73ffb380c3c6
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-08-05 15:09:08 +0900

    collection-query 분리 1. 진행중인 이전 query 실행 취소 2. 만약, undefined 상태라면 초기화까지

---
+++ src/hooks/use-cancel-query.ts
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+import { useQueryClient } from "react-query";
+
+export const useCancelQuery = queryKey => {
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    return () => {
+      queryKey.forEach(key => {
+        // key의 query 중단
+        if (queryClient.isFetching(key)) queryClient.cancelQueries(key);
+
+        // key에 대한 캐싱된 값 초기화
+        if (!queryClient.getQueryData(key))
+          queryClient.resetQueries(key, { exact: true });
+      });
+    };
+  }, [queryClient, queryKey]);
+};
