868978d983cb721e6658752e6e4a7f0db1266e92
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2022-04-23 15:45:19 +0900

    feat: public 디렉토리 제거

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
 	plugins: [react(), tsconfigPaths()],
+	base: '/loa-hands/',
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src'),
