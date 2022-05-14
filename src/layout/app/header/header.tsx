97dcd8fa45ff9d2ae2574fdfb7835bd7af88c11a
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2022-05-14 10:58:15 +0900

    feat(react-update): 타입 수정

--- src/layout/app/header/header.tsx
+++ src/layout/app/header/header.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useRef } from 'react';
+import React, { FormEvent, MouseEvent, useCallback, useRef } from 'react';
 import { useQueryErrorResetBoundary } from 'react-query';
 import { useNavigate } from 'react-router-dom';
 
@@ -12,7 +12,7 @@ function Header({ ...props }) {
 	const navigate = useNavigate();
 
 	const handleSubmit = useCallback(
-		(e) => {
+		(e: FormEvent<HTMLFormElement>) => {
 			e.preventDefault();
 			const { current } = textInput;
 
@@ -22,47 +22,53 @@ function Header({ ...props }) {
 
 			if (isEmpty) return;
 			reset();
-			e.target.search.disabled = true;
-			e.target.search.value = null;
-			navigate(`/userInfo/${name}`, { replace: true });
+			(e.target as HTMLFormElement).search.disabled = true;
+			(e.target as HTMLFormElement).search.value = null;
+			navigate(`/userInfo/${name}?test=userInfo`, { replace: true });
 		},
 		[textInput, navigate, reset],
 	);
 
 	const handleGoHome = useCallback(
-		(e) => {
+		(e: MouseEvent) => {
 			e.preventDefault();
 			reset();
-			navigate(`/`, { replace: true });
+			navigate(`/?test=home`, { replace: true });
 		},
 		[navigate, reset],
 	);
 
-	const handleUnDisabled = useCallback((e) => {
-		if (!e.target.disabled) return;
-		e.target.disabled = false;
+	const handleUnDisabled = useCallback((e: MouseEvent) => {
+		if (!(e.target as HTMLInputElement).disabled) return;
+		(e.target as HTMLInputElement).disabled = false;
 	}, []);
 
-  return (
-    <Styled.Container {...props}>
-      <Styled.Logo onClick={handleGoHome}>
-        <Styled.Background data-testid="go-home" />
-      </Styled.Logo>
-      <Styled.Form data-testid="submit-form" onSubmit={handleSubmit}>
-        <Styled.InputText
-          onMouseOverCapture={handleUnDisabled}
-          data-testid="search-area"
-          type="text"
-          name="search"
-          ref={textInput}
-          autoComplete="off"
-        />
-        <Styled.InputSubmit type="submit" buttonType="submit">
-          <Text>검색</Text>
-        </Styled.InputSubmit>
-      </Styled.Form>
-    </Styled.Container>
-  );
+	return (
+		<Styled.Container {...props}>
+			<Styled.Logo onClick={handleGoHome}>
+				<Styled.Background data-testid="go-home" />
+			</Styled.Logo>
+			<Styled.Form
+				data-testid="submit-form"
+				onSubmit={handleSubmit}
+			>
+				<Styled.InputText
+					onMouseOverCapture={handleUnDisabled}
+					data-testid="search-area"
+					type="text"
+					name="search"
+					ref={textInput}
+					autoComplete="off"
+				/>
+				<Styled.InputSubmit
+					type="submit"
+					buttonType="submit"
+				>
+					<Text>검색</Text>
+				</Styled.InputSubmit>
+			</Styled.Form>
+		</Styled.Container>
+	);
 }
 
 export default React.memo(Header);
