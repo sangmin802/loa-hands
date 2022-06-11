84ba112e20430e6592122ea350495a232633edcc
박상민 <56113148+sangmin802@users.noreply.github.com>
2022-06-12 08:47:44 +0900

    feat(web-vitals): 콘솔 병합

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,22 @@ import '@/style/fonts.css';
 
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { getCLS, getFID, getLCP } from 'web-vitals';
 
 import { URL } from '@/constants/env';
 import QueryClientContext from '@/context/queryClientContext';
 import StyleContext from '@/context/styleContext';
 import App from '@/layout/app';
+
+getCLS(console.log);
+getFID(console.log);
+getLCP(console.log);
+
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+console.log(5);
 
 createRoot(document.getElementById('root') as HTMLElement).render(
 	<Router basename={URL}>
