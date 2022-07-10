1f5e6ea443b9f89ad39c7c61d9d1d628883d2482
박상민 <56113148+sangmin802@users.noreply.github.com>
2022-07-10 11:16:28 +0900

    feat(update) (#47)
    * feat(event): 모든 이벤트가 보이도록 수정 (#46)

--- src/models/eventData.ts
+++ src/models/eventData.ts
@@ -17,30 +17,36 @@ export default class EventData {
 		href: string;
 	}[] = [];
 
-	constructor(events: string) {
+	constructor(events: string[]) {
 		// 이벤트리스트
-		this.setEvents(returnBody(events));
+
+		events.forEach((event) => this.setEvents(returnBody(event)));
 	}
 
 	setEvents(raw: Element) {
 		const eventList =
 			raw.getElementsByClassName('list list--event')[0].children[0].children;
 
-		this.events = [...eventList].map((li) => {
-			const href = (li.children[0].attributes as Attr)['href'].value;
-			const children = li.children[0].children;
-			const img = (children[0].children[0].attributes as Attr)['src'].value;
-			const name = children[1].children[0].textContent as string;
-			const date = (children[2].children[0].childNodes[1].textContent as string)
-				.trim()
-				.replace(': ', '');
+		this.events = [
+			...this.events,
+			...[...eventList].map((li) => {
+				const href = (li.children[0].attributes as Attr)['href'].value;
+				const children = li.children[0].children;
+				const img = (children[0].children[0].attributes as Attr)['src'].value;
+				const name = children[1].children[0].textContent as string;
+				const date = (
+					children[2].children[0].childNodes[1].textContent as string
+				)
+					.trim()
+					.replace(': ', '');
 
-			return {
-				img,
-				name,
-				date,
-				href,
-			};
-		});
+				return {
+					img,
+					name,
+					date,
+					href,
+				};
+			}),
+		];
 	}
 }
